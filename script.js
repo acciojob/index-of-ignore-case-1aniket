022f6f8fa99ed7d108f2d1f6c22c4aa742b0b2a8
Aniket Bhogawar <39596152+1aniket@users.noreply.github.com>
2024-12-23 12:13:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	let s2Size=s2.length;
+	s1=s1.toUpperCase();
+	s2=s2.toUpperCase();
+
+	for(let i=0;i<=s1.length-s2Size;i++)
+	{
+
+		if(s1.substr(i , s2Size).toUpperCase()== s2.toUpperCase())
+		{
+		  
+			return i;
+		}
+			
+	}
+	
+	return -1;
 }
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
+
